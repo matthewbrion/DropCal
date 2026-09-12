3a918216e4d29920fb2d9cbd454284c2757f63f1
Matt <matthewbrion@me.com>
2026-09-12 12:00:08 -0400

    added protected route notes to log and readme, built Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function AppRoutes() {
   const { loading } = useAuth();
@@ -12,6 +14,9 @@ function AppRoutes() {
     <Routes>
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path='/' element={<Home />} />
+      </Route>
     </Routes>
   );
 }
